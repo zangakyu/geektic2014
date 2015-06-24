edf08fccce9859e5c7e2a119f2588cd8577a1e06
jbnizet <jb@ninja-squad.com>
2015-06-24 09:26:00 +0200

    Add OpenEntityManagerInView interceptor

--- src/main/java/com/ninja_squad/geektic/config/AppConfig.java
+++ src/main/java/com/ninja_squad/geektic/config/AppConfig.java
@@ -1,5 +1,7 @@
 package com.ninja_squad.geektic.config;
 
+import javax.sql.DataSource;
+
 import org.hibernate.jpa.HibernatePersistenceProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -9,13 +11,13 @@ import org.springframework.context.annotation.Import;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.support.OpenEntityManagerInViewInterceptor;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-import javax.sql.DataSource;
 
 /**
  * Spring application configuration. Lacks some annotations.
@@ -58,5 +60,15 @@ public class AppConfig extends WebMvcConfigurerAdapter {
         JpaTransactionManager txManager = new JpaTransactionManager();
         txManager.setEntityManagerFactory(emf().getObject());
         return txManager;
+    }
+    
+    @Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addWebRequestInterceptor(openEntityManagerInViewInterceptor());
+	}
+
+	@Bean
+    public OpenEntityManagerInViewInterceptor openEntityManagerInViewInterceptor() {
+    	return new OpenEntityManagerInViewInterceptor();
     }
 }
