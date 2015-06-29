a54b3db310013e2023d47e4f989a0e29ce72efc4
zangakyu <zangakyu@Wind-PC.fr>
2015-06-29 16:09:39 +0200

    Angular geeks route impl

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -1,16 +1,16 @@
 var app = angular.module("geektic", ['ngRoute']);
 
-app.config(['$routeProvider', function ($routeProvider) {
+app.config(function ($routeProvider) {
         $routeProvider.
-            when('/api/geeks', {templateUrl:'views/geeks.html',   controller:'GeekticCtrl'}).
-            when('/api/search',  {templateUrl:'views/search.html',    controller:'GeekticCtrl'}).
-            when('/api/geek',  {templateUrl:'views/geek.html',    controller:'GeekticCtrl'}).
+            when('/geeks', {templateUrl:'views/geeks.html',   controller:'GeekticCtrl'}).
+            when('/search',  {templateUrl:'views/search.html',    controller:'GeekticCtrl'}).
+            when('/geek',  {templateUrl:'views/geek.html',    controller:'GeekticCtrl'}).
             otherwise({redirectTo:'/'});
 });
 
 app.controller('GeekticCtrl', function($scope, $http) {
-    $http.get('/api/hello').success(function(helloMessage) {
-        $scope.helloMessage = helloMessage;
+    $http.get('/api/geeks').success(function(geeks) {
+        $scope.geeks=geeks;
     });
 });
 app.controller('HelloCtrl', function($scope, $http) {
